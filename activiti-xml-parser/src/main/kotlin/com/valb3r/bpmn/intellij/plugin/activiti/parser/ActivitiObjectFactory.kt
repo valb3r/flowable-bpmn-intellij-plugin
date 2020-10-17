f6cd00f7156c7f0fae273c7ea103fa990677644d
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-17 21:26:00 +0300

    FBP-191. Discard unsupported properties

--- activiti-xml-parser/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/activiti/parser/ActivitiObjectFactory.kt
+++ activiti-xml-parser/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/activiti/parser/ActivitiObjectFactory.kt
@@ -164,6 +164,14 @@ class ActivitiObjectFactory: BpmnObjectFactory {
             }
         }
 
+        result.remove(PropertyType.HEADERS)
+        result.remove(PropertyType.IS_USE_LOCAL_SCOPE_FOR_RESULT_VARIABLE)
+        result.remove(PropertyType.IS_TRIGGERABLE)
+        result.remove(PropertyType.DECISION_TASK_THROW_ERROR_ON_NO_HITS)
+        result.remove(PropertyType.FALLBACK_TO_DEF_TENANT_CDATA)
+        result.remove(PropertyType.FORM_FIELD_VALIDATION)
+        result.remove(PropertyType.CATEGORY)
+        result.remove(PropertyType.SKIP_EXPRESSION)
         return result
     }
 
