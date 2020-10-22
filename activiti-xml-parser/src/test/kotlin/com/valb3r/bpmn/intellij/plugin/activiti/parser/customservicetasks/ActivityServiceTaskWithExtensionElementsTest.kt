4a63ddb73b43b84abc1c3d0452f771f61de2654a
valb3r <valentyn.berezin@adorsys.com.ua>
2020-10-22 13:40:46 +0300

    FBP-196. Added tests for update

--- activiti-xml-parser/src/test/kotlin/com/valb3r/bpmn/intellij/plugin/activiti/parser/customservicetasks/ActivityServiceTaskWithExtensionElementsTest.kt
+++ activiti-xml-parser/src/test/kotlin/com/valb3r/bpmn/intellij/plugin/activiti/parser/customservicetasks/ActivityServiceTaskWithExtensionElementsTest.kt
@@ -1,9 +1,16 @@
 package com.valb3r.bpmn.intellij.plugin.activiti.parser.customservicetasks
 
+import com.valb3r.bpmn.intellij.plugin.activiti.parser.ActivitiObjectFactory
 import com.valb3r.bpmn.intellij.plugin.activiti.parser.ActivitiParser
 import com.valb3r.bpmn.intellij.plugin.activiti.parser.asResource
+import com.valb3r.bpmn.intellij.plugin.activiti.parser.readAndUpdateProcess
+import com.valb3r.bpmn.intellij.plugin.activiti.parser.testevents.StringValueUpdatedEvent
+import com.valb3r.bpmn.intellij.plugin.bpmn.api.BpmnProcessObject
+import com.valb3r.bpmn.intellij.plugin.bpmn.api.bpmn.BpmnElementId
+import com.valb3r.bpmn.intellij.plugin.bpmn.api.bpmn.elements.tasks.BpmnServiceTask
+import com.valb3r.bpmn.intellij.plugin.bpmn.api.info.PropertyType
 import org.amshove.kluent.shouldBeEqualTo
-import org.amshove.kluent.shouldNotBeEmpty
+import org.amshove.kluent.shouldHaveSingleItem
 import org.junit.jupiter.api.Test
 
 private const val FILE = "custom-service-tasks/service-task-with-extension.bpmn20.xml"
@@ -11,12 +18,30 @@ private const val FILE = "custom-service-tasks/service-task-with-extension.bpmn2
 internal class ActivityServiceTaskWithExtensionElementsTest {
 
     private val parser = ActivitiParser()
+    private val elementId = BpmnElementId("serviceTaskWithExtensionId")
 
     @Test
     fun `Service task with failedJobRetryTimeCycle is parseable`() {
         val processObject = parser.parse(FILE.asResource()!!)
+        val task = processObject.process.body!!.serviceTask!![0]
 
-        processObject.process.body!!.serviceTask!![0].extensionElements!!.shouldNotBeEmpty()
-        processObject.process.body!!.serviceTask!![0].extensionElements!![0].failedJobRetryTimeCycle.shouldBeEqualTo("R10/PT5M")
+        task.id.shouldBeEqualTo(elementId)
+        task.failedJobRetryTimeCycleExtension.shouldBeEqualTo("R10/PT5M")
+
+        val props = BpmnProcessObject(processObject.process, processObject.diagram).toView(ActivitiObjectFactory()).elemPropertiesByElementId[task.id]!!
+        props[PropertyType.FAILED_JOB_RETRY_CYCLE]!!.value.shouldBeEqualTo(task.failedJobRetryTimeCycleExtension)
+    }
+
+    @Test
+    fun `Service task failedJobRetryTimeCycle is updatable`() {
+        {value: String -> readAndUpdate(PropertyType.FAILED_JOB_RETRY_CYCLE, value).failedJobRetryTimeCycleExtension.shouldBeEqualTo(value)} ("PT999/12");
+    }
+
+    private fun readAndUpdate(property: PropertyType, newValue: String): BpmnServiceTask {
+        return readServiceTask(readAndUpdateProcess(parser, FILE, StringValueUpdatedEvent(elementId, property, newValue)))
+    }
+
+    private fun readServiceTask(processObject: BpmnProcessObject): BpmnServiceTask {
+        return processObject.process.body!!.serviceTask!!.shouldHaveSingleItem()
     }
 }
