7fc39128801b7278490766422ea99833662755c7
Valentyn Berezin <vbe@adorsys.com.ua>
2021-02-28 21:58:55 +0200

    FBP-163. Added TAB/Shift+TAB support

--- bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/ui/components/MultiEditJTable.kt
+++ bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/ui/components/MultiEditJTable.kt
@@ -8,8 +8,11 @@ import com.intellij.ui.table.JBTable
 import com.intellij.util.ui.AbstractTableCellEditor
 import java.awt.Component
 import java.awt.Font
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
 import javax.swing.JLabel
 import javax.swing.JTable
+import javax.swing.KeyStroke
 import javax.swing.table.DefaultTableModel
 import javax.swing.table.TableCellEditor
 import javax.swing.table.TableCellRenderer
@@ -19,6 +22,10 @@ import kotlin.math.max
 
 class MultiEditJTable(tableModel: TableModel): JBTable(tableModel) {
     private val intraCellSpacingWidth = 10
+
+    init {
+        attachShiftAndShiftTabActions()
+    }
 
     override fun getCellEditor(row: Int, column: Int): TableCellEditor {
         return when (val value = modelValue(row, column)) {
@@ -54,6 +61,29 @@ class MultiEditJTable(tableModel: TableModel): JBTable(tableModel) {
         val modelRow = convertRowIndexToModel(row)
         val modelColumn = convertColumnIndexToModel(column)
         return super.dataModel.getValueAt(modelRow, modelColumn)
+    }
+
+    private fun attachShiftAndShiftTabActions() {
+        val nextRow = this.getInputMap(WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).get(KeyStroke.getKeyStroke("TAB"))
+        val prevRow = this.getInputMap(WHEN_ANCESTOR_OF_FOCUSED_COMPONENT).get(KeyStroke.getKeyStroke("shift pressed TAB"))
+        if (null == nextRow || null == prevRow) {
+            return
+        }
+
+        this.actionMap.put(nextRow, object : AbstractAction() {
+            override fun actionPerformed(e: ActionEvent) {
+                val newRow = if (selectedRow == rowCount - 1) 0 else selectedRow + 1
+                changeSelection(newRow, selectedColumn, false, false)
+                editCellAt(newRow, selectedColumn)
+            }
+        })
+        this.actionMap.put(prevRow, object : AbstractAction() {
+            override fun actionPerformed(e: ActionEvent) {
+                val newRow = if (selectedRow == 0) rowCount - 1 else selectedRow - 1
+                changeSelection(newRow, selectedColumn, false, false)
+                editCellAt(newRow, selectedColumn)
+            }
+        })
     }
 }
 
