81bfdf20d23e84e507c4ee70a2b9fa3cab10f5b8
valb3r <valb3r.ua@gmail.com>
2021-07-23 11:02:18 +0300

    FBP-233. Added dialog to select diagram-image name

--- bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/actions/DiagramToPng.kt
+++ bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/actions/DiagramToPng.kt
@@ -1,12 +1,16 @@
 package com.valb3r.bpmn.intellij.plugin.core.actions
 
+import com.intellij.openapi.fileChooser.FileChooserFactory
+import com.intellij.openapi.fileChooser.FileSaverDescriptor
 import com.intellij.openapi.project.Project
 import com.valb3r.bpmn.intellij.plugin.core.render.currentCanvas
-import java.io.File
 import javax.imageio.ImageIO
 
 fun saveDiagramToPng(project: Project) {
     val canvas = currentCanvas(project)
     val image = canvas.renderToBitmap() ?: return
-    ImageIO.write(image, "png", File("/home/valb3r/test.png"))
+    val descriptor = FileSaverDescriptor("Save Diagram To", "Save diagram to file", "png")
+    val dialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, project)
+    val dest = dialog.save(null, "diagram") ?: return
+    ImageIO.write(image, "png", dest.file)
 }
