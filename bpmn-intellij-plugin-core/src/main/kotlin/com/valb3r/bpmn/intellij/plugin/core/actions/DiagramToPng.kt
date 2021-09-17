5ec6b6a65cb584d58aa04804de3bf2cf1a987707
valb3r <valb3r.ua@gmail.com>
2021-09-17 11:13:37 +0300

    FBP-245. Fix build issue

--- bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/actions/DiagramToPng.kt
+++ bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/actions/DiagramToPng.kt
@@ -3,6 +3,7 @@ package com.valb3r.bpmn.intellij.plugin.core.actions
 import com.intellij.openapi.fileChooser.FileChooserFactory
 import com.intellij.openapi.fileChooser.FileSaverDescriptor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.valb3r.bpmn.intellij.plugin.core.render.currentCanvas
 import java.nio.file.Path
 import javax.imageio.ImageIO
@@ -12,6 +13,6 @@ fun saveDiagramToPng(project: Project) {
     val image = canvas.renderToBitmap() ?: return
     val descriptor = FileSaverDescriptor("Save Diagram To", "Save diagram to file", "png")
     val dialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, project)
-    val dest = dialog.save(null as Path?, "diagram") ?: return
+    val dest = dialog.save(null as VirtualFile?, "diagram") ?: return
     ImageIO.write(image, "png", dest.file)
 }
