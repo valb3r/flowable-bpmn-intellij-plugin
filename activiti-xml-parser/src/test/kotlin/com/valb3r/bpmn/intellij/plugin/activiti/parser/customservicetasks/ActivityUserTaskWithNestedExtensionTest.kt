9e001ff8dfaeb63b131cb01938b5cca173d83581
valb3r <valb3r.ua@gmail.com>
2021-10-10 00:15:15 +0300

    FBP-235. Tests for support deep nested form elements

--- activiti-xml-parser/src/test/kotlin/com/valb3r/bpmn/intellij/plugin/activiti/parser/customservicetasks/ActivityUserTaskWithNestedExtensionTest.kt
+++ activiti-xml-parser/src/test/kotlin/com/valb3r/bpmn/intellij/plugin/activiti/parser/customservicetasks/ActivityUserTaskWithNestedExtensionTest.kt
@@ -4,16 +4,16 @@ import com.valb3r.bpmn.intellij.plugin.activiti.parser.*
 import com.valb3r.bpmn.intellij.plugin.activiti.parser.testevents.StringValueUpdatedEvent
 import com.valb3r.bpmn.intellij.plugin.bpmn.api.BpmnProcessObject
 import com.valb3r.bpmn.intellij.plugin.bpmn.api.bpmn.BpmnElementId
-import com.valb3r.bpmn.intellij.plugin.bpmn.api.bpmn.elements.tasks.BpmnServiceTask
 import com.valb3r.bpmn.intellij.plugin.bpmn.api.bpmn.elements.tasks.BpmnUserTask
+import com.valb3r.bpmn.intellij.plugin.bpmn.api.info.FunctionalGroupType
+import com.valb3r.bpmn.intellij.plugin.bpmn.api.info.Property
 import com.valb3r.bpmn.intellij.plugin.bpmn.api.info.PropertyType
+import com.valb3r.bpmn.intellij.plugin.bpmn.api.info.PropertyValueType
 import org.amshove.kluent.*
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 private const val FILE = "custom-service-tasks/user-task-with-nested-extensions.bpmn20.xml"
 
-@Disabled
 internal class ActivityUserTaskWithNestedExtensionTest {
 
     private val parser = ActivitiParser()
@@ -32,31 +32,71 @@ internal class ActivityUserTaskWithNestedExtensionTest {
         props[PropertyType.ID]!!.value.shouldBeEqualTo(task.id.id)
         props[PropertyType.NAME]!!.value.shouldBeEqualTo(task.name)
         props[PropertyType.DOCUMENTATION]!!.value.shouldBeEqualTo(task.documentation)
+
+        props.getAll(PropertyType.FORM_PROPERTY_ID).shouldContainSame(arrayOf(
+            Property("fooVariableName", listOf("fooVariableName")), Property("isSkippable", listOf("isSkippable")), Property("fullProperty", listOf("fullProperty"))
+        ))
+        props.getAll(PropertyType.FORM_PROPERTY_NAME).shouldContainSame(arrayOf(
+            Property(null, listOf("fooVariableName")), Property(null, listOf("isSkippable")), Property("Full Property", listOf("fullProperty"))
+        ))
+        props.getAll(PropertyType.FORM_PROPERTY_TYPE).shouldContainSame(arrayOf(
+            Property("string", listOf("fooVariableName")), Property("boolean", listOf("isSkippable")), Property("string", listOf("fullProperty"))
+        ))
+        props.getAll(PropertyType.FORM_PROPERTY_VARIABLE).shouldContainSame(arrayOf(
+            Property("fooDefault", listOf("fooVariableName")), Property(null, listOf("isSkippable")), Property("fullProperty", listOf("fullProperty"))
+        ))
+        props.getAll(PropertyType.FORM_PROPERTY_DEFAULT).shouldContainSame(arrayOf(
+            Property("isFoo", listOf("fooVariableName")), Property(null, listOf("isSkippable")), Property("aValue", listOf("fullProperty"))
+        ))
+        props.getAll(PropertyType.FORM_PROPERTY_EXPRESSION).shouldContainSame(arrayOf(
+            Property(null, listOf("fooVariableName")), Property("\${userCanSkip}", listOf("isSkippable")), Property("\${foo}", listOf("fullProperty"))
+        ))
+        props.getAll(PropertyType.FORM_PROPERTY_DATE_PATTERN).shouldContainSame(arrayOf(
+            Property(null, listOf("fooVariableName")), Property(null, listOf("isSkippable")), Property("DD.MM.YYYY", listOf("fullProperty"))
+        ))
+        props.getAll(PropertyType.FORM_PROPERTY_VALUE_ID).shouldContainSame(arrayOf(
+            Property(null, listOf("fooVariableName")), Property(null, listOf("isSkippable")), Property("value1", listOf("fullProperty", "value1")), Property("value2", listOf("fullProperty", "value2"))
+        ))
+        props.getAll(PropertyType.FORM_PROPERTY_VALUE_NAME).shouldContainSame(arrayOf(
+            Property(null, listOf("fooVariableName")), Property(null, listOf("isSkippable")), Property("Foo", listOf("fullProperty", "value1")), Property("Bar", listOf("fullProperty", "value2"))
+        ))
     }
 
     @Test
     fun `User task nested elements are updatable`() {
-        {value: String -> readAndUpdate(PropertyType.FIELD_NAME, value, "recipient").formPropertiesExtension!![0].name.shouldBeEqualTo(value)} ("new name");
-        {value: String -> readAndUpdate(PropertyType.FIELD_EXPRESSION, value, "recipient").formPropertiesExtension!![0].expression.shouldBeEqualTo(value)} ("EXPR0=EXPR1");
-        {value: String -> readAndUpdate(PropertyType.FIELD_NAME, value, "multiline").formPropertiesExtension!![1].name.shouldBeEqualTo(value)} ("new mulitline name");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_ID, value, "fooVariableName").formPropertiesExtension!![0].id.shouldBeEqualTo(value)} ("new id");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_NAME, value, "fooVariableName").formPropertiesExtension!![0].name.shouldBeEqualTo(value)} ("new name");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_TYPE, value, "fooVariableName").formPropertiesExtension!![0].type.shouldBeEqualTo(value)} ("new type");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_VARIABLE, value, "fooVariableName").formPropertiesExtension!![0].variable.shouldBeEqualTo(value)} ("new variable");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_DEFAULT, value, "fooVariableName").formPropertiesExtension!![0].default.shouldBeEqualTo(value)} ("new default");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_EXPRESSION, value, "fooVariableName").formPropertiesExtension!![0].expression.shouldBeEqualTo(value)} ("new expression");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_DATE_PATTERN, value, "fooVariableName").formPropertiesExtension!![0].datePattern.shouldBeEqualTo(value)} ("new datePattern");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_VALUE_ID, value, "fooVariableName,").formPropertiesExtension!![0].value!![0].id?.shouldBeEqualTo(value)} ("new inner id");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_VALUE_NAME, value, "fooVariableName,").formPropertiesExtension!![0].value!![0].name.shouldBeEqualTo(value)} ("new inner name");
     }
 
     @Test
     fun `Service task nested elements are emptyable`() {
-        readAndSetNullStringAndAssertItIsRemoved(PropertyType.FIELD_NAME, "recipient", "activiti:field name=\"recipient\"", "<activiti:field>", "<activiti:field/>", "activiti:expression")
-            .formPropertiesExtension?.map { it.name }?.shouldNotContain("recipient")
-        readAndSetNullStringAndAssertItIsRemoved(PropertyType.FIELD_EXPRESSION, "recipient", "activiti:expression").formPropertiesExtension!![0].expression.shouldBeNullOrEmpty()
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_ID, value, "fullProperty").formPropertiesExtension?.shouldHaveSize(2)} ("");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_NAME, value, "fullProperty").formPropertiesExtension!![2].name.shouldBeNull()} ("");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_TYPE, value, "fullProperty").formPropertiesExtension!![2].type.shouldBeNull()} ("");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_VARIABLE, value, "fullProperty").formPropertiesExtension!![2].variable.shouldBeNull()} ("");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_DEFAULT, value, "fullProperty").formPropertiesExtension!![2].default.shouldBeNull()} ("");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_EXPRESSION, value, "fullProperty").formPropertiesExtension!![2].expression.shouldBeNull()} ("");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_DATE_PATTERN, value, "fullProperty").formPropertiesExtension!![2].datePattern.shouldBeNull()} ("");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_VALUE_ID, value, "fullProperty,value1").formPropertiesExtension!![2].value!![0].id?.shouldBeNull()} ("");
+        {value: String -> readAndUpdate(PropertyType.FORM_PROPERTY_VALUE_NAME, value, "fullProperty,value1").formPropertiesExtension!![2].value!![0].name.shouldBeNull()} ("");
     }
 
     private fun readAndSetNullStringAndAssertItIsRemoved(property: PropertyType, propertyIndex: String, vararg shouldNotContainStr: String): BpmnUserTask {
-        val event = StringValueUpdatedEvent(elementId, property, "", propertyIndex = listOf(propertyIndex))
+        val event = StringValueUpdatedEvent(elementId, property, "", propertyIndex = propertyIndex.split(","))
         val updated = updateBpmnFile(parser, FILE, listOf(event))
         shouldNotContainStr.forEach { updated.shouldNotContain(it) }
         return readUserTaskWithExtensions(parser.parse(updated))
     }
 
     private fun readAndUpdate(property: PropertyType, newValue: String, propertyIndex: String): BpmnUserTask {
-        return readUserTaskWithExtensions(readAndUpdateProcess(parser, FILE, StringValueUpdatedEvent(elementId, property, newValue, propertyIndex = listOf(propertyIndex))))
+        return readUserTaskWithExtensions(readAndUpdateProcess(parser, FILE, StringValueUpdatedEvent(elementId, property, newValue, propertyIndex = propertyIndex.split(","))))
     }
 
     private fun readUserTaskWithExtensions(processObject: BpmnProcessObject): BpmnUserTask {
