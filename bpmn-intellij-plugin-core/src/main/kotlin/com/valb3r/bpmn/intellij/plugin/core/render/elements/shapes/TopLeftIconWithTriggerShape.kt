1e179ddf81f482283328b645b5dda2e0650348af
vova <vova.stepanenko2020@gmail.com>
2022-05-31 10:02:17 +0300

    minor fix

--- bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/render/elements/shapes/TopLeftIconWithTriggerShape.kt
+++ bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/render/elements/shapes/TopLeftIconWithTriggerShape.kt
@@ -20,7 +20,7 @@ class TopLeftIconWithTriggerShape(
     shape: ShapeElement,
     state: () -> RenderState,
     backgroundColor: Colors = Colors.SERVICE_TASK_COLOR,
-    borderColor: Colors =  Colors.ELEMENT_BORDER_COLOR,
+    borderColor: Colors = Colors.ELEMENT_BORDER_COLOR,
     textColor: Colors = Colors.INNER_TEXT_COLOR
 ) : TopLeftIconShape(elementId, bpmnElementId, icon, shape, state, backgroundColor, borderColor, textColor) {
 
@@ -30,15 +30,12 @@ class TopLeftIconWithTriggerShape(
         if (!isTriggered()) {
             return area
         }
-
-        val triggerElementId = DiagramElementId("TRIGGER:$elementId")
-        val trigger = ctx.canvas.drawTriggered(shapeCtx.shape, state().icons.triggered)
-        area[triggerElementId] =
-            AreaWithZindex(trigger, AreaType.POINT, mutableSetOf(), mutableSetOf(), ICON_Z_INDEX, elementId)
+        ctx.canvas.drawTriggered(shapeCtx.shape, state().icons.triggered)
         return area
     }
 
     private fun isTriggered(): Boolean {
-        return (state().currentState.elemPropertiesByStaticElementId[bpmnElementId]?.get(PropertyType.IS_TRIGGERABLE)?.value as Boolean? ?: false)
+        return (state().currentState.elemPropertiesByStaticElementId[bpmnElementId]?.get(PropertyType.IS_TRIGGERABLE)?.value as Boolean?
+            ?: false)
     }
 }
