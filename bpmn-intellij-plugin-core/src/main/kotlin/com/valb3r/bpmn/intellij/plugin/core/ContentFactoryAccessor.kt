a407efa9284375bc37fe47994d12b86f7a24104f
valb3r <valb3r.ua@gmail.com>
2022-12-17 16:39:48 +0200

    FBP-327. Bump content factory access to be compatible

--- bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/ContentFactoryAccessor.kt
+++ bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/ContentFactoryAccessor.kt
@@ -5,9 +5,9 @@ import com.intellij.ui.content.ContentFactory
 // TODO !COMPATIBILITY: (< 2022) IntelliJ provides only ContentFactory.SERVICE.getInstance(). This method is scheduled for removal
 fun getContentFactory(): ContentFactory {
     return try {
-        val contentFactorySupplier = ContentFactory::class.java.getField("SERVICE")
-        contentFactorySupplier.get(null) as ContentFactory
-    } catch (ex: NoSuchFieldException) {
+        val serviceClazz = ContentFactory::class.java.classes.find { it.simpleName == "SERVICE" }!!
+        return serviceClazz.getDeclaredMethod("getInstance").invoke(null) as ContentFactory
+    } catch (ex: Exception) {
         // New IntelliJ provides only ContentFactory.getInstance()
         val contentFactorySupplier = ContentFactory::class.java.getMethod("getInstance")
         contentFactorySupplier.invoke(null) as ContentFactory
