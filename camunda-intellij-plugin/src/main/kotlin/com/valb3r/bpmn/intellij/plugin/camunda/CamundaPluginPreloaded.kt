bd00704837cfa06cf712d43ce5761138a2614251
valb3r <valb3r.ua@gmail.com>
2022-12-18 12:47:28 +0200

    FBP-327. Bump preloaded activity

--- camunda-intellij-plugin/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/camunda/CamundaPluginPreloaded.kt
+++ camunda-intellij-plugin/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/camunda/CamundaPluginPreloaded.kt
@@ -1,14 +1,19 @@
 package com.valb3r.bpmn.intellij.plugin.camunda
 
-import com.intellij.openapi.application.PreloadingActivity
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.progress.ProgressIndicator
-import com.valb3r.bpmn.intellij.plugin.core.settings.currentSettingsStateProvider
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
 import com.valb3r.bpmn.intellij.plugin.camunda.settings.CamundaBpmnPluginSettingsState
+import com.valb3r.bpmn.intellij.plugin.core.settings.currentSettingsStateProvider
+import java.util.concurrent.atomic.AtomicBoolean
 
-class CamundaPluginPreloaded: PreloadingActivity() {
+class CamundaPluginPreloaded: StartupActivity.Background {
 
-    override fun preload(indicator: ProgressIndicator) {
-        currentSettingsStateProvider.set { ServiceManager.getService(CamundaBpmnPluginSettingsState::class.java) }
+    private val isLoaded = AtomicBoolean()
+
+    override fun runActivity(project: Project) {
+        if (isLoaded.compareAndSet(false, true)) {
+            currentSettingsStateProvider.set { ServiceManager.getService(CamundaBpmnPluginSettingsState::class.java) }
+        }
     }
 }
