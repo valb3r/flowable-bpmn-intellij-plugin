98f21ddb5b99dbc2535d810be61c9ab84d111822
valb3r <valb3r.ua@gmail.com>
2023-05-24 09:02:01 +0300

    Anchor example

--- bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/render/elements/anchors/IconAnchorElement.kt
+++ bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/render/elements/anchors/IconAnchorElement.kt
@@ -5,7 +5,9 @@ import com.valb3r.bpmn.intellij.plugin.bpmn.api.diagram.DiagramElementId
 import com.valb3r.bpmn.intellij.plugin.core.Colors
 import com.valb3r.bpmn.intellij.plugin.core.render.AreaType
 import com.valb3r.bpmn.intellij.plugin.core.render.AreaWithZindex
+import com.valb3r.bpmn.intellij.plugin.core.render.Camera
 import com.valb3r.bpmn.intellij.plugin.core.render.RenderContext
+import com.valb3r.bpmn.intellij.plugin.core.render.elements.Anchor
 import com.valb3r.bpmn.intellij.plugin.core.render.elements.RenderState
 import java.awt.geom.Point2D
 import javax.swing.Icon
@@ -18,7 +20,14 @@ abstract class IconAnchorElement(
 ): AnchorElement(elementId, attachedTo, currentLocation, state) {
 
     override val areaType: AreaType
-        get() = AreaType.POINT
+        get() = AreaType.SHAPE
+
+    override fun shapeAnchors(camera: Camera): MutableSet<Anchor> {
+        val rect = currentOnScreenRect(camera)
+        val cx = rect.x + rect.width / 2.0f
+        val cy = rect.y + rect.height / 2.0f
+        return mutableSetOf(Anchor(Point2D.Float(cx, cy)))
+    }
 
     override fun doRenderWithoutChildren(ctx: RenderContext): Map<DiagramElementId, AreaWithZindex> {
         val icon = icon()
@@ -30,7 +39,7 @@ abstract class IconAnchorElement(
             dx: Float, dy: Float, droppedOn: BpmnElementId?, allDroppedOn: Map<BpmnElementId, AreaWithZindex> -> onDragEnd(dx, dy, droppedOn, allDroppedOn)
         }
 
-        return mutableMapOf(elementId to AreaWithZindex(area, areaType, index = zIndex()))
+        return mutableMapOf(elementId to AreaWithZindex(area, areaType, index = zIndex(), anchorsForShape = shapeAnchors(ctx.canvas.camera)))
     }
 
     protected abstract fun icon(): Icon
