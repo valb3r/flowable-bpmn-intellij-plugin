1106240a2adcf5d5b9e40a022358b76a422b88fc
valb3r <valb3r.ua@gmail.com>
2023-06-30 13:08:13 +0300

    Fixed messages

--- bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/advertisement/notificationTryPolyBpmn.kt
+++ bpmn-intellij-plugin-core/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/core/advertisement/notificationTryPolyBpmn.kt
@@ -11,7 +11,7 @@ fun showTryPolyBpmnAdvertisementNotification(project: Project) {
     val frequencyCheck = 30L
     if(checkDate == LocalDate.MIN || checkDate.plusDays(frequencyCheck).isBefore(now)) {
         AdvertisementState.getInstance().lastShowCommon = now
-        genericShowNotificationBalloon(project, "Advertisement", "Try <a href='https://plugins.jetbrains.com/plugin/21361-polybpmn-visualizer'>PolyBPMN plugin</a>. <br> PolyBPMN's upgraded split editor to each file, revision file comparison and a wider selection of elements and properties..", NotificationType.INFORMATION, "Do not show again") {
+        genericShowNotificationBalloon(project, "Advertisement", "Try <a href='https://plugins.jetbrains.com/plugin/21361-polybpmn-visualizer'>PolyBPMN plugin</a>. <br> PolyBPMN's has upgraded split editor for each file, diagram history and a wider selection of elements and properties.", NotificationType.INFORMATION, "Do not show again") {
             doNotShowAgain(project, frequencyCheck) // set maximum date
         }
     }
@@ -21,10 +21,9 @@ fun showTryPolyBpmnAdvertisementSwimpoolNotification(project: Project) {
     val checkDate = AdvertisementState.getInstance().lastShowSwimpoolAd
     val now = LocalDate.now()
     val frequencyCheck = 30L
-    if(checkDate == LocalDate.MIN || checkDate.plusDays(frequencyCheck).isBefore(now)) {
+    if (checkDate == LocalDate.MIN || checkDate.plusDays(frequencyCheck).isBefore(now)) {
         AdvertisementState.getInstance().lastShowSwimpoolAd = now
-        genericShowNotificationBalloon(project, "Advertisement Swimpool", "Unlock the full potential of diagram reading and editiong with the PolyBPMN plugin. Seamlessly visualize and interpret swimpools and swimlanes with ease. Dwonload from <a href='https://plugins.jetbrains.com/plugin/21361-polybpmn-visualizer'>here</a>.\n" +
-                "Unlock the full potential of diagram reading and editiong with the PolyBPMN plugin. Seamlessly visualize and interpret swimpools and swimlanes with ease. Dwonload from <a href='https://plugins.jetbrains.com/plugin/21361-polybpmn-visualizer'>here</a>.", NotificationType.INFORMATION, "Do not show again") {
+        genericShowNotificationBalloon(project, "Advertisement Swimming pool", "Unlock the full potential of diagram reading and editing with the <a href='https://plugins.jetbrains.com/plugin/21361-polybpmn-visualizer'>PolyBPMN plugin</a>. Seamlessly visualize and interpret swimming pools and swimming lanes with ease. Download from <a href='https://plugins.jetbrains.com/plugin/21361-polybpmn-visualizer'>here</a>", NotificationType.INFORMATION, "Do not show again") {
             doNotShowAgain(project, frequencyCheck) // set maximum date
         }
     }
