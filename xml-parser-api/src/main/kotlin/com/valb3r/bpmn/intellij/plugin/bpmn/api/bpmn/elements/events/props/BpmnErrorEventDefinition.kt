c2275c853152beb24e533830579aac61e8d1a61b
martin-grofcik <mgrofcik@gmail.com>
2025-02-28 10:24:07 +0100

    #320 errorVariable name, localScope, transient ErrorEventDefinition attributes

--- xml-parser-api/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/bpmn/api/bpmn/elements/events/props/BpmnErrorEventDefinition.kt
+++ xml-parser-api/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/bpmn/api/bpmn/elements/events/props/BpmnErrorEventDefinition.kt
@@ -1,5 +1,9 @@
 package com.valb3r.bpmn.intellij.plugin.bpmn.api.bpmn.elements.events.props
 
 data class BpmnErrorEventDefinition(
-    val errorRef: String?
-)
+    val errorRef: String?,
+    val errorVariableName: String?,
+    val errorVariableLocalScope: Boolean?,
+    val errorVariableTransient: Boolean?
+) {
+}
