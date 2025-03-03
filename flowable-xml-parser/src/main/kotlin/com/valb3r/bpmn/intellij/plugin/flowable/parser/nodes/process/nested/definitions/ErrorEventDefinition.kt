d95f41b1adc59e822e31da210bb02538919769bb
martin-grofcik <mgrofcik@gmail.com>
2025-03-03 09:03:08 +0100

    #320 CR findings fix

--- flowable-xml-parser/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/flowable/parser/nodes/process/nested/definitions/ErrorEventDefinition.kt
+++ flowable-xml-parser/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/flowable/parser/nodes/process/nested/definitions/ErrorEventDefinition.kt
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 
 data class ErrorEventDefinition(
     @JacksonXmlProperty(isAttribute = true) val errorRef: String? = null,
-    @JacksonXmlProperty(isAttribute = true, namespace = "http://flowable.org/bpmn") val errorVariableName: String? = null,
-    @JacksonXmlProperty(isAttribute = true, namespace = "http://flowable.org/bpmn") val errorVariableLocalScope: Boolean? = null,
-    @JacksonXmlProperty(isAttribute = true, namespace = "http://flowable.org/bpmn") val errorVariableTransient: Boolean? = null
+    @JacksonXmlProperty(isAttribute = true) val errorVariableName: String? = null,
+    @JacksonXmlProperty(isAttribute = true) val errorVariableLocalScope: Boolean? = null,
+    @JacksonXmlProperty(isAttribute = true) val errorVariableTransient: Boolean? = null
 )
