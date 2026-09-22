dd334ae0316dcfc51a0dc7e95ac96ddbf3943e9c
valentyn.berezin <valb3r.ua@gmail.com>
2026-09-22 14:30:57 +0300

    Fix for init

--- camunda-intellij-plugin/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/camunda/CamundaBpmnPluginToolWindowFactory.kt
+++ camunda-intellij-plugin/src/main/kotlin/com/valb3r/bpmn/intellij/plugin/camunda/CamundaBpmnPluginToolWindowFactory.kt
@@ -52,7 +52,7 @@ class CamundaBpmnPluginToolWindowFactory: ToolWindowFactory {
 
         // register the tool window content
         val content = getContentFactory().createContent(
-                bpmnWindow.getContent(),
+                bpmnWindow.createMainToolWindowPanel(),
                 "",
                 false
         )
